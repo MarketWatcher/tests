13ef5ca8c57023dbc35553ca0157df0376e78748
yigithanyilmaz <yyilmaz@thoughtworks.com>
2016-06-12 15:03:54 +0300

    #3 Story @yigithanyilmaz bug fix on scoping

--- user_login_test.go
+++ user_login_test.go
@@ -4,9 +4,10 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/sclevine/agouti"
-	//. "github.com/sclevine/agouti/matchers"
+	. "github.com/sclevine/agouti/matchers"
 )
 
+// Configuration
 var _ = Describe("UserLogin", func() {
 	var page *agouti.Page
 
@@ -15,26 +16,26 @@ var _ = Describe("UserLogin", func() {
 		page, err = agoutiDriver.NewPage()
 		Expect(err).NotTo(HaveOccurred())
 
-		var loginPage = map[string]string{
-			"email":    "loginEmailAddress",
-			"password": "exampleInputPassword1",
-			"submit":   "Submit",
-		}
-
 	})
+
+	var loginPage = map[string]string{
+		"email":    "loginEmailAddress",
+		"password": "exampleInputPassword1",
+		"submit":   "Submit",
+	}
 
 	AfterEach(func() {
 		Expect(page.Destroy()).To(Succeed())
 	})
-
+	// Test Feature
 	Describe("User Login Authentication", func() {
-
+		// Acceptence Criterias
 		It("should allow the user to dashboard with correct username and password and display username", func() {
 
 			Expect(page.Find(loginPage["email"]).Fill("user1@mail.com"))
 			Expect(page.Find(loginPage["password"]).Fill("passOne")).To(Succeed())
 			Expect(page.FindByButton(loginPage["submit"]).Submit()).To(Succeed())
-			Eventually(profile.Find(".greeting")).Should(HaveText("user 1"))
+			Eventually(page.Find(".greeting")).Should(HaveText("user 1"))
 
 		})
 
@@ -42,21 +43,21 @@ var _ = Describe("UserLogin", func() {
 
 			Expect(page.Find(loginPage["email"]).Fill("user1@mail.com"))
 			Expect(page.Find(loginPage["password"]).Fill("wrongPassword")).To(Succeed())
-			Expect(page.FindByButton(loginPage["submit"]).Submit()).To(Succeed())
+			Expect(page.FindByButton(loginPage["submit"]).Submit())
 		})
 
 		It("should not allowing the user to dashboard with wrong username and correct password", func() {
 
 			Expect(page.Find(loginPage["email"]).Fill("notauser@mail.com"))
 			Expect(page.Find(loginPage["password"]).Fill("passOne")).To(Succeed())
-			Expect(page.FindByButton(loginPage["submit"]).Submit()).To(Succeed())
+			Expect(page.FindByButton(loginPage["submit"]).Submit())
 		})
 
 		It("should not allowing the user to dashboard with null username and password", func() {
 
 			Expect(page.Find(loginPage["email"]).Fill(" "))
 			Expect(page.Find(loginPage["password"]).Fill(" ")).To(Succeed())
-			Expect(page.FindByButton(loginPage["submit"]).Submit()).To(Succeed())
+			Expect(page.FindByButton(loginPage["submit"]).Submit())
 		})
 
 	})
