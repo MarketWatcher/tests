212b9bf34fdcf1073569abc12e2aa0eb4a391650
Berna Gokce <bernagokce92@gmail.com>
2016-06-27 15:54:06 +0300

    #13 adds new acceptance criterias for create alarm test

--- create_alarm_test.go
+++ create_alarm_test.go
@@ -60,7 +60,7 @@ var _ = FDescribe("CreateAlarm", func() {
 			//empty alarm
 			Expect(page.Find(HomePage["createButton"]).Click()).To(Succeed())
 			Expect(page.Find(AlarmPage["alertname"]).Fill("")).To(Succeed())
-			Expect(page.Find(AlarmPage["required"]).Fill(" TW, ThoughtWorks, Thought Works, Thoughtworks")).To(Succeed())
+			Expect(page.Find(AlarmPage["required"]).Fill("TW, ThoughtWorks, Thought Works, Thoughtworks")).To(Succeed())
 			Expect(page.Find(AlarmPage["niceToHave"]).Fill("good, best office")).To(Succeed())
 			Expect(page.Find(AlarmPage["excluded"]).Fill("sucks, not good enough")).To(Succeed())
 			Expect(page.Find(AlarmPage["threshold"]).Fill("1000")).To(Succeed())
@@ -205,4 +205,205 @@ var _ = FDescribe("CreateAlarm", func() {
 		})
 
 	})
+
+	Describe("When user logged in and usertry to create alarm succesfully", func() {
+		BeforeEach(func() {
+			//login
+			Expect(page.Navigate(LoginPage["url"])).To(Succeed())
+			Expect(page.Find(LoginPage["email"]).Fill("user1@mail.com")).To(Succeed())
+			Expect(page.Find(LoginPage["password"]).Fill("passOne")).To(Succeed())
+			Expect(page.FindByButton(LoginPage["submit"]).Submit()).To(Succeed())
+		})
+
+		It("should user can not create alarm with nonalphanumaric alert header", func() {
+			//alarm 1
+			Expect(page.Find(HomePage["createButton"]).Click()).To(Succeed())
+			Expect(page.Find(AlarmPage["alertname"]).Fill("%&^*^&*&*8")).To(Succeed())
+			Expect(page.Find(AlarmPage["required"]).Fill(" TW, ThoughtWorks, Thought Works, Thoughtworks")).To(Succeed())
+			Expect(page.Find(AlarmPage["niceToHave"]).Fill("good, best office")).To(Succeed())
+			Expect(page.Find(AlarmPage["excluded"]).Fill("sucks, not good enough")).To(Succeed())
+			Expect(page.Find(AlarmPage["threshold"]).Fill("1000")).To(Succeed())
+			Expect(page.Find(AlarmPage["saveButton"]).Click()).To(Succeed())
+			Eventually(page.Find(AlarmPage["alertnameError"])).Should(HaveText("\"Alert title\" must be alphanumeric"))
+		})
+
+	})
+
+	Describe("When user logged in and usertry to create alarm succesfully", func() {
+		BeforeEach(func() {
+			//login
+			Expect(page.Navigate(LoginPage["url"])).To(Succeed())
+			Expect(page.Find(LoginPage["email"]).Fill("user1@mail.com")).To(Succeed())
+			Expect(page.Find(LoginPage["password"]).Fill("passOne")).To(Succeed())
+			Expect(page.FindByButton(LoginPage["submit"]).Submit()).To(Succeed())
+		})
+
+		It("should user can not create alarm with more than 32 unicode characters alert header", func() {
+			//alarm 1
+			Expect(page.Find(HomePage["createButton"]).Click()).To(Succeed())
+			Expect(page.Find(AlarmPage["alertname"]).Fill("fnserkjgfnsekrgjnvkesjrngvkserjngvs")).To(Succeed())
+			Expect(page.Find(AlarmPage["required"]).Fill(" TW, ThoughtWorks, Thought Works, Thoughtworks")).To(Succeed())
+			Expect(page.Find(AlarmPage["niceToHave"]).Fill("good, best office")).To(Succeed())
+			Expect(page.Find(AlarmPage["excluded"]).Fill("sucks, not good enough")).To(Succeed())
+			Expect(page.Find(AlarmPage["threshold"]).Fill("1000")).To(Succeed())
+			Expect(page.Find(AlarmPage["saveButton"]).Click()).To(Succeed())
+			Eventually(page.Find(AlarmPage["alertnameError"])).Should(HaveText("\"Alert title\" length must be less than or equal to 32 characters long"))
+		})
+
+	})
+	Describe("When user logged in and user try to create alarm succesfully", func() {
+		BeforeEach(func() {
+			//login
+			Expect(page.Navigate(LoginPage["url"])).To(Succeed())
+			Expect(page.Find(LoginPage["email"]).Fill("user1@mail.com")).To(Succeed())
+			Expect(page.Find(LoginPage["password"]).Fill("passOne")).To(Succeed())
+			Expect(page.FindByButton(LoginPage["submit"]).Submit()).To(Succeed())
+		})
+
+		It("should user create alarm succesfully with turkish characters in header", func() {
+			//alarm 1
+			Expect(page.Find(HomePage["createButton"]).Click()).To(Succeed())
+			Expect(page.Find(AlarmPage["alertname"]).Fill("ğüğççççööş")).To(Succeed())
+			Expect(page.Find(AlarmPage["required"]).Fill(" TW, ThoughtWorks, Thought Works, Thoughtworks")).To(Succeed())
+			Expect(page.Find(AlarmPage["niceToHave"]).Fill("good, best office")).To(Succeed())
+			Expect(page.Find(AlarmPage["excluded"]).Fill("sucks, not good enough")).To(Succeed())
+			Expect(page.Find(AlarmPage["threshold"]).Fill("1000")).To(Succeed())
+			Expect(page.Find(AlarmPage["saveButton"]).Click()).To(Succeed())
+			Eventually(page.Find(HomePage["successMessage"])).Should(HaveText("Alert was created"))
+		})
+	})
+
+	Describe("When user logged in and user try to create alarm succesfully", func() {
+		BeforeEach(func() {
+			//login
+			Expect(page.Navigate(LoginPage["url"])).To(Succeed())
+			Expect(page.Find(LoginPage["email"]).Fill("user1@mail.com")).To(Succeed())
+			Expect(page.Find(LoginPage["password"]).Fill("passOne")).To(Succeed())
+			Expect(page.FindByButton(LoginPage["submit"]).Submit()).To(Succeed())
+		})
+
+		It("should user can not create alarm succesfully with more than 140 characterts for required field", func() {
+			//alarm 1
+			Expect(page.Find(HomePage["createButton"]).Click()).To(Succeed())
+			Expect(page.Find(AlarmPage["alertname"]).Fill("Alert 1")).To(Succeed())
+			Expect(page.Find(AlarmPage["required"]).Fill("fnserkjgfnsekrgjnvkesjrngvkserjngvsdfjcdnsfkjsndfknsdvndjvbdjfbdjsbfgdjshgbejshbgjfnkksjfnekjsfnejrfnenjfneefjnehrbfejrhgberhjgwdfjebdjeshfbe")).To(Succeed())
+			Expect(page.Find(AlarmPage["niceToHave"]).Fill("good, best office")).To(Succeed())
+			Expect(page.Find(AlarmPage["excluded"]).Fill("sucks, not good enough")).To(Succeed())
+			Expect(page.Find(AlarmPage["threshold"]).Fill("1000")).To(Succeed())
+			Expect(page.Find(AlarmPage["saveButton"]).Click()).To(Succeed())
+			Eventually(page.Find(AlarmPage["requiredError"])).Should(HaveText("\"Must include\" length must be less than or equal to 140 characters long"))
+		})
+	})
+	Describe("When user logged in and user try to create alarm succesfully", func() {
+		BeforeEach(func() {
+			//login
+			Expect(page.Navigate(LoginPage["url"])).To(Succeed())
+			Expect(page.Find(LoginPage["email"]).Fill("user1@mail.com")).To(Succeed())
+			Expect(page.Find(LoginPage["password"]).Fill("passOne")).To(Succeed())
+			Expect(page.FindByButton(LoginPage["submit"]).Submit()).To(Succeed())
+		})
+
+		It("should user can not create alarm succesfully with non unicode required", func() {
+			//alarm 1
+			Expect(page.Find(HomePage["createButton"]).Click()).To(Succeed())
+			Expect(page.Find(AlarmPage["alertname"]).Fill("Alert 1")).To(Succeed())
+			Expect(page.Find(AlarmPage["required"]).Fill("%$^%^&^*&^*^")).To(Succeed())
+			Expect(page.Find(AlarmPage["niceToHave"]).Fill("good, best office")).To(Succeed())
+			Expect(page.Find(AlarmPage["excluded"]).Fill("sucks, not good enough")).To(Succeed())
+			Expect(page.Find(AlarmPage["threshold"]).Fill("1000")).To(Succeed())
+			Expect(page.Find(AlarmPage["saveButton"]).Click()).To(Succeed())
+			Eventually(page.Find(AlarmPage["requiredError"])).Should(HaveText("\"Must include\" must be alphanumeric"))
+		})
+	})
+
+	Describe("When user logged in and user try to create alarm succesfully", func() {
+		BeforeEach(func() {
+			//login
+			Expect(page.Navigate(LoginPage["url"])).To(Succeed())
+			Expect(page.Find(LoginPage["email"]).Fill("user1@mail.com")).To(Succeed())
+			Expect(page.Find(LoginPage["password"]).Fill("passOne")).To(Succeed())
+			Expect(page.FindByButton(LoginPage["submit"]).Submit()).To(Succeed())
+		})
+
+		It("should user can not create alarm succesfully with non unicode characterts for niceToHave", func() {
+			//alarm 1
+			Expect(page.Find(HomePage["createButton"]).Click()).To(Succeed())
+			Expect(page.Find(AlarmPage["alertname"]).Fill("Alarm 1")).To(Succeed())
+			Expect(page.Find(AlarmPage["required"]).Fill(" TW, ThoughtWorks, Thought Works, Thoughtworks")).To(Succeed())
+			Expect(page.Find(AlarmPage["niceToHave"]).Fill("%^&^&*&^*")).To(Succeed())
+			Expect(page.Find(AlarmPage["excluded"]).Fill("sucks, not good enough")).To(Succeed())
+			Expect(page.Find(AlarmPage["threshold"]).Fill("1000")).To(Succeed())
+			Expect(page.Find(AlarmPage["saveButton"]).Click()).To(Succeed())
+			Eventually(page.Find(AlarmPage["niceToHaveError"])).Should(HaveText("\"Can include\" must be alphanumeric"))
+		})
+
+	})
+
+	Describe("When user logged in and user try to create alarm succesfully", func() {
+		BeforeEach(func() {
+			//login
+			Expect(page.Navigate(LoginPage["url"])).To(Succeed())
+			Expect(page.Find(LoginPage["email"]).Fill("user1@mail.com")).To(Succeed())
+			Expect(page.Find(LoginPage["password"]).Fill("passOne")).To(Succeed())
+			Expect(page.FindByButton(LoginPage["submit"]).Submit()).To(Succeed())
+		})
+
+		It("should user can not create alarm succesfully with 140 characterts for niceToHave", func() {
+			//alarm 1
+			Expect(page.Find(HomePage["createButton"]).Click()).To(Succeed())
+			Expect(page.Find(AlarmPage["alertname"]).Fill("Alarm 1")).To(Succeed())
+			Expect(page.Find(AlarmPage["required"]).Fill(" TW, ThoughtWorks, Thought Works, Thoughtworks")).To(Succeed())
+			Expect(page.Find(AlarmPage["niceToHave"]).Fill("fnserkjgfnsekrgjnvkesjrngvkserjngvsdfjcdnsfkjsndfknsdvndjvbdjfbdjsbfgdjshgbejshbgjfnkksjfnekjsfnejrfnenjfneefjnehrbfejrhgberhjgwdfjebdjeshfbe")).To(Succeed())
+			Expect(page.Find(AlarmPage["excluded"]).Fill("sucks, not good enough")).To(Succeed())
+			Expect(page.Find(AlarmPage["threshold"]).Fill("1000")).To(Succeed())
+			Expect(page.Find(AlarmPage["saveButton"]).Click()).To(Succeed())
+			Eventually(page.Find(AlarmPage["niceToHaveError"])).Should(HaveText("\"Can include\" length must be less than or equal to 140 characters long"))
+		})
+
+	})
+	Describe("When user logged in and user try to create alarm succesfully", func() {
+		BeforeEach(func() {
+			//login
+			Expect(page.Navigate(LoginPage["url"])).To(Succeed())
+			Expect(page.Find(LoginPage["email"]).Fill("user1@mail.com")).To(Succeed())
+			Expect(page.Find(LoginPage["password"]).Fill("passOne")).To(Succeed())
+			Expect(page.FindByButton(LoginPage["submit"]).Submit()).To(Succeed())
+		})
+
+		It("should user can not create alarm succesfully with 140 characterts for excluded", func() {
+			//alarm 1
+			Expect(page.Find(HomePage["createButton"]).Click()).To(Succeed())
+			Expect(page.Find(AlarmPage["alertname"]).Fill("Alarm 1")).To(Succeed())
+			Expect(page.Find(AlarmPage["required"]).Fill(" TW, ThoughtWorks, Thought Works, Thoughtworks")).To(Succeed())
+			Expect(page.Find(AlarmPage["niceToHave"]).Fill("nice, good")).To(Succeed())
+			Expect(page.Find(AlarmPage["excluded"]).Fill("fnserkjgfnsekrgjnvkesjrngvkserjngvsdfjcdnsfkjsndfknsdvndjvbdjfbdjsbfgdjshgbejshbgjfnkksjfnekjsfnejrfnenjfneefjnehrbfejrhgberhjgwdfjebdjeshfbe")).To(Succeed())
+			Expect(page.Find(AlarmPage["threshold"]).Fill("1000")).To(Succeed())
+			Expect(page.Find(AlarmPage["saveButton"]).Click()).To(Succeed())
+			Eventually(page.Find(AlarmPage["excludedError"])).Should(HaveText("\"Exclude criteria\" length must be less than or equal to 140 characters long"))
+
+		})
+	})
+	Describe("When user logged in and user try to create alarm succesfully", func() {
+		BeforeEach(func() {
+			//login
+			Expect(page.Navigate(LoginPage["url"])).To(Succeed())
+			Expect(page.Find(LoginPage["email"]).Fill("user1@mail.com")).To(Succeed())
+			Expect(page.Find(LoginPage["password"]).Fill("passOne")).To(Succeed())
+			Expect(page.FindByButton(LoginPage["submit"]).Submit()).To(Succeed())
+		})
+
+		It("should user can not create alarm succesfully with non unicode characterts for excluded", func() {
+			//alarm 1
+			Expect(page.Find(HomePage["createButton"]).Click()).To(Succeed())
+			Expect(page.Find(AlarmPage["alertname"]).Fill("Alarm 1")).To(Succeed())
+			Expect(page.Find(AlarmPage["required"]).Fill(" TW, ThoughtWorks, Thought Works, Thoughtworks")).To(Succeed())
+			Expect(page.Find(AlarmPage["niceToHave"]).Fill("nice, good")).To(Succeed())
+			Expect(page.Find(AlarmPage["excluded"]).Fill("%^&%&%^&*^&*")).To(Succeed())
+			Expect(page.Find(AlarmPage["threshold"]).Fill("1000")).To(Succeed())
+			Expect(page.Find(AlarmPage["saveButton"]).Click()).To(Succeed())
+			Eventually(page.Find(AlarmPage["excludedError"])).Should(HaveText("\"Exclude criteria\" must be alphanumeric"))
+
+		})
+	})
+
 })
