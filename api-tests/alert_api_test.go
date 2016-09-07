100099307d21a492d8ce476994416d204e760abf
Ömer Faruk Almali <omerfaruk.almali@gmail.com>
2016-09-07 14:38:06 +0300

    #0 @ayseyo @bernagokce add getById api test

--- api-tests/alert_api_test.go
+++ api-tests/alert_api_test.go
@@ -3,12 +3,15 @@ package api
 import (
 	"net/http"
 	"testing"
-
 	"github.com/gavv/httpexpect"
 	"os"
-	"fmt"
-	"net/http/httptest"
+	"time"
+	"math/rand"
 )
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
 
 func TestAlertCreationWithEmptyRequestBody(t *testing.T) {
 	e := httpexpect.New(t, os.Getenv("ALERT_SERVICE_URL"))
@@ -39,7 +42,7 @@ func TestAlertCreationWithEmptyTitle(t *testing.T) {
 func TestSuccesfulAlertCreation(t *testing.T) {
 	alert := map[string]interface{}{
 		"owner_id":1,
-		"name": "namee",
+		"name": RandomWord(10),
 		"required_criteria":"aaa",
 		"nice_to_have_criteria":"fff",
 		"excluded_criteria":"ddd",
@@ -58,7 +61,7 @@ func TestSuccesfulAlertCreation(t *testing.T) {
 func TestAlertCreationWithEmptyThreshold(t *testing.T) {
 	alert := map[string]interface{}{
 		"owner_id":1,
-		"name": "namee",
+		"name": RandomWord(10),
 		"required_criteria":"aaa",
 		"nice_to_have_criteria":"fff",
 		"excluded_criteria":"ddd",
@@ -72,11 +75,38 @@ func TestAlertCreationWithEmptyThreshold(t *testing.T) {
 		Status(http.StatusBadRequest)
 }
 
+func TestGetAlertWithAlertId(t *testing.T){
+	name := RandomWord(10)
+	alert := map[string]interface{}{
+		"owner_id":2,
+		"name": name,
+		"required_criteria":"aab",
+		"nice_to_have_criteria":"ffe",
+		"excluded_criteria":"ddr",
+		"threshold":555,
+		"status":23,
+	}
+	e := httpexpect.New(t, os.Getenv("ALERT_SERVICE_URL"))
+
+	savedAlert := e.POST("/api/alerts").WithJSON(alert).
+		Expect().
+		Status(http.StatusOK).
+		JSON().Object()
+
+	getById := 	"/api/alerts/id/" + savedAlert.Value("id").Raw().(string)
+	e.GET(getById).
+	Expect().
+	Status(http.StatusOK).
+	JSON().Object().
+	ContainsKey("name").
+	ValueEqual("name", name)
+}
+
 func TestAlertCreationWithEmptyMust(t *testing.T) {
 
 	alert := map[string]interface{} {
 		"owner_id":1,
-		"name": "namee",
+		"name": RandomWord(10),
 		"nice_to_have_criteria":"fff",
 		"excluded_criteria":"ddd",
 		"threshold":555,
@@ -84,8 +114,19 @@ func TestAlertCreationWithEmptyMust(t *testing.T) {
 	}
 
 	e := httpexpect.New(t, os.Getenv("ALERT_SERVICE_URL"))
+
 	e.POST("/api/alerts").WithJSON(alert).
 		Expect().
 		Status(http.StatusBadRequest)
 
+}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func RandomWord(n int) string {
+    b := make([]rune, n)
+    for i := range b {
+        b[i] = letterRunes[rand.Intn(len(letterRunes))]
+    }
+    return string(b)
 }
